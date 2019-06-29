025d48d79f092264906c785c2171c3871b7a8352
Igori4 <igorpilyak@gmail.com>
2019-06-30 01:58:26 +0300

    Updates

---
+++ precache-manifest.82b48aa40ae99cda210c73511812ba95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edc5958903e62e14ffda770b6b7ef05",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "6f9ef3b1084b349677b0",
+    "url": "/react_dynamic-list-of-todos/static/css/main.ab3dd7bb.chunk.css"
+  },
+  {
+    "revision": "80df47b08caa5cd7a6d8",
+    "url": "/react_dynamic-list-of-todos/static/js/2.17dcfcec.chunk.js"
+  },
+  {
+    "revision": "6f9ef3b1084b349677b0",
+    "url": "/react_dynamic-list-of-todos/static/js/main.ad5b862d.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
